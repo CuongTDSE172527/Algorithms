da9f8213b4f54a857630f5112b527dc1a590b5b6
cuongjackfruit <cuongtdse172527@fpt.edu.vn>
2024-04-03 20:56:54 +0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int* a, int* b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap(&arr[j], &arr[j+1]);
+            }
+        }
+    }
+}
+
+void selectionSort(int arr[], int n) {
+    int min_idx;
+    for (int i = 0; i < n-1; i++) {
+        min_idx = i;
+        for (int j = i+1; j < n; j++) {
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        }
+        swap(&arr[min_idx], &arr[i]);
+    }
+}
+
+void insertionSort(int arr[], int n) {
+    int key, j;
+    for (int i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void merge(int arr[], int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {5, 2, 9, 0, 3, 7, 1, 4, 8, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original Array: ");
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+    printf("Sorted array using Bubble Sort: ");
+    printArray(arr, n);
+
+    int arr2[] = {5, 2, 9, 0, 3, 7, 1, 4, 8, 6};
+    selectionSort(arr2, n);
+    printf("Sorted array using Selection Sort: ");
+    printArray(arr2, n);
+
+    int arr3[] = {5, 2, 9, 0, 3, 7, 1, 4, 8, 6};
+    insertionSort(arr3, n);
+    printf("Sorted array using Insertion Sort: ");
+    printArray(arr3, n);
+
+    int arr4[] = {5, 2, 9, 0, 3, 7, 1, 4, 8, 6};
+    mergeSort(arr4, 0, n - 1);
+    printf("Sorted array using Merge Sort: ");
+    printArray(arr4, n);
+
+    int arr5[] = {5, 2, 9, 0, 3, 7, 1, 4, 8, 6};
+    quickSort(arr5, 0, n - 1);
+    printf("Sorted array using Quick Sort: ");
+    printArray(arr5, n);
+
+    return 0;
+}
